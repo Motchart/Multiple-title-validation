70782f45eae88553ddb817103387021a1268d413
motchart <motchart007@gmail.com>
2016-11-19 18:06:33 -0800

    syntax corection

--- src/main/java/core/HTMLUnit.java
+++ src/main/java/core/HTMLUnit.java
@@ -13,7 +13,7 @@ public class HTMLUnit {
 		}
 		WebDriver driver = new FirefoxDriver();
 		for (int i = 0; i < v.length; i++) {
-			String text_case_id = "TC-001.0" + (i + 1);
+			String test_case_id = "TC-001.0" + (i + 1);
 			String param[] = v[i].split("\\|");
 			String url = param[0];
 			String title_expected = param[1];
@@ -23,13 +23,13 @@ public class HTMLUnit {
 			String title_actual = driver.getTitle();
 			System.out.println("");
 			if(title_expected.equals(title_actual)) {
-				System.out.println("Test Case ID:\t\t" + text_case_id);
+				System.out.println("Test Case ID:\t\t" + test_case_id);
 				System.out.println("URL: \t\t\t" + url);
 				System.out.println("Title Expected: \t" + title_expected);
 				System.out.println("Title Actual: \t\t" + title_actual);
 				System.out.println("Test Case Result: \t" + "PASSED");
 			}else {
-				System.out.println("Test Case ID:\t\t" + text_case_id);
+				System.out.println("Test Case ID:\t\t" + test_case_id);
 				System.out.println("URL: \t\t\t" + url);
 				System.out.println("Title Expected: \t" + title_expected);
 				System.out.println("Title Actual: \t\t" + title_actual);
